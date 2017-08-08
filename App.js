903d4197739b3acc9b5a27d5341c293783afa10f
Mohamed Osman <mothman198@outlook.com>
2017-08-08 22:48:10 +0800

    added different icons

--- App.js
+++ App.js
@@ -7,6 +7,55 @@ export default class App extends React.Component {
 
 onRegionChange(region) {
   this.setState({ region });
+  if(region.latitudeDelta>0.1){
+    this.state.markers=[
+         {
+          id:0,
+          latlng:{ latitude: 2.945895,longitude: 101.870711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"013-sun-umbrella.png",
+          pic: require('./assets/icons/png/013-sun-umbrella.png')
+         },
+        {
+          id:1,
+          latlng:{ latitude: 2.945895,longitude: 101.874711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"013-sun-umbrella.png",
+          pic: require('./assets/icons/png/013-sun-umbrella.png')
+         }
+    ]
+  }
+  else {
+    this.state.markers=[
+         {
+          id:0,
+          latlng:{ latitude: 2.945895,longitude: 101.870711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"013-sun-umbrella.png",
+          pic: require('./assets/icons/png/013-sun-umbrella.png')
+         },
+        {
+          id:1,
+          latlng:{ latitude: 2.945895,longitude: 101.874711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"013-sun-umbrella.png",
+          pic: require('./assets/icons/png/013-sun-umbrella.png')
+         },
+         {
+          id:2,
+          latlng:{ latitude: 2.945895,longitude: 101.878711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"002-shopping-cart.png",
+          pic: require('./assets/icons/png/002-shopping-cart.png')
+         },
+       ]
+  }
+
 
 }
   
@@ -23,10 +72,28 @@ onRegionChange(region) {
        mapStyle:[{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#6195a0"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":"0"},{"saturation":"0"},{"color":"#f5f5f2"},{"gamma":"1"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"lightness":"-3"},{"gamma":"1.00"}]},{"featureType":"landscape.natural.terrain","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#bae5ce"},{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#fac9a9"},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#787878"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit.station.airport","elementType":"labels.icon","stylers":[{"hue":"#0a00ff"},{"saturation":"-77"},{"gamma":"0.57"},{"lightness":"0"}]},{"featureType":"transit.station.rail","elementType":"labels.text.fill","stylers":[{"color":"#43321e"}]},{"featureType":"transit.station.rail","elementType":"labels.icon","stylers":[{"hue":"#ff6c00"},{"lightness":"4"},{"gamma":"0.75"},{"saturation":"-68"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#eaf6f8"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c7eced"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"lightness":"-49"},{"saturation":"-53"},{"gamma":"0.79"}]}],
        markers:[
          {
-          
+          id:0,
           latlng:{ latitude: 2.945895,longitude: 101.870711},
           title:"University Of Nottingham",
-          description:"The university Of nottingham in malaysia"
+          description:"The university Of nottingham in malaysia",
+          icon:"013-sun-umbrella.png",
+          pic: require('./assets/icons/png/013-sun-umbrella.png')
+         },
+        {
+          id:1,
+          latlng:{ latitude: 2.945895,longitude: 101.874711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"013-sun-umbrella.png",
+          pic: require('./assets/icons/png/013-sun-umbrella.png')
+         },
+         {
+          id:2,
+          latlng:{ latitude: 2.945895,longitude: 101.878711},
+          title:"University Of Nottingham",
+          description:"The university Of nottingham in malaysia",
+          icon:"002-shopping-cart.png",
+          pic: require('./assets/icons/png/002-shopping-cart.png')
          },
        ]
 
@@ -51,18 +118,21 @@ render() {
      
     <View style={styles.container}>
      <MapView
+      rotateEnabled={false}
        showsUserLocation={true}
+       showsCompass={false}
        style={styles.map}
        region={this.state.region}
        onRegionChange={this.onRegionChange}
        customMapStyle={this.state.mapStyle}>
        
          {this.state.markers.map(marker => (
+
     <MapView.Marker
+    key={marker.id}
       coordinate={marker.latlng}
       title={marker.title}
-      image={require('../assets/icons/app-icon.png')}
-
+      image={marker.pic}
       description={marker.description}
     />
   ))}
